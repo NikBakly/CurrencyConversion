8a61b72e833bd6cf19ed8f348426bc4dbc79aa8d
baklyNik <baklykov.nikita369@gmail.com>
2023-08-07 17:21:29 +0300

    refactoring table db

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,6 +1,6 @@
 create table if not exists currencies
 (
-    id        int primary key,
+    id        integer primary key autoincrement,
     code      varchar(255),
     full_name varchar(255),
     sign      varchar(4)
@@ -9,9 +9,9 @@ create table if not exists currencies
 
 create table if not exists exchange_rates
 (
-    id                 int primary key,
-    base_currency_id   int not null,
-    target_currency_id int not null,
+    id                 integer primary key autoincrement,
+    base_currency_id   integer not null,
+    target_currency_id integer not null,
     rate               decimal(6),
     foreign key (base_currency_id) references currencies (id),
     foreign key (target_currency_id) references currencies (id)
